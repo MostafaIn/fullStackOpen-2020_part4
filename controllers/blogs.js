6a46398107c6a4e8fcf6a17f0d72fc74a851c3e6
mostafaIn <mostafa2015se@gmail.com>
2020-06-24 22:05:51 +0300

    Exercises 4.1.-4.2.

---
+++ controllers/blogs.js
@@ -0,0 +1,25 @@
+const blogRouter = require('express').Router()
+const Blog = require('../models/Blog')
+
+blogRouter.get('/',(req, res) =>{
+    Blog.find({}).then( blogs =>{
+        res.json(blogs.map( blog => blog.toJSON()))
+    })
+});
+
+blogRouter.post('/', (req, res, next) =>{
+    const body = req.body
+    const blog = new Blog({
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes
+    });
+
+    blog.save()
+    .then( savedBlog => res.json(savedBlog.toJSON()))
+    .catch( err => next(err))
+});
+
+
+module.exports = blogRouter
