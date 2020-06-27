af8bf88f4db295901969b7a028270e6e049b5d17
mostafaIn <mostafa2015se@gmail.com>
2020-06-27 13:48:11 +0300

    4.4: helper functions and unit tests, step2

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -3,6 +3,11 @@ const dummy = blogs => {
   return 1
 }
 
+const totalLikes = list => {
+  return list.reduce((sum, blog) => sum + blog.likes, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
