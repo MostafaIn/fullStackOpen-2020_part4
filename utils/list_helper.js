dafb95ec9064cbd4f8b1a8e639ecdab55d0251cd
mostafaIn <mostafa2015se@gmail.com>
2020-06-27 16:22:26 +0300

    4.5*: helper functions and unit tests, step3

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -7,7 +7,12 @@ const totalLikes = list => {
   return list.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
+const favoriteBlog = list => {
+  return list.find( blog => blog.likes === Math.max(...list.map( bl => bl.likes)))
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
