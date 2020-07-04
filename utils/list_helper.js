71ecb10b85e554b87c26431bfe814d55d18ccc28
mostafaIn <mostafa2015se@gmail.com>
2020-07-04 14:55:08 +0300

    4.6*: helper functions and unit tests, step4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -11,8 +11,24 @@ const favoriteBlog = list => {
   return list.find( blog => blog.likes === Math.max(...list.map( bl => bl.likes)))
 }
 
+const mostBlogs = list => {
+  if (list.length < 1) {
+    return null
+  }
+  const authors = list.reduce( (acc, cur) => {
+    acc[cur.author] = ++ acc[cur.author] || 1
+    return acc
+  },{})
+
+  let blogs = Math.max(...Object.values(authors))
+  let author = Object.keys(authors).find( author => authors[author] === blogs)
+
+  return { author, blogs }
+}
+
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs
 }
